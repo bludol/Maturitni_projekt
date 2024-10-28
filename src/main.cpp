7877dff3081a4c10c971a9d8a0e334eace702e73
bludol <bludol.bludol@gmail.com>
2024-10-28 16:29:46 +0100

    + Přidání vodní pumpy spouštěné pomocí  Endpointu /togglePump

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
-#include <SimpleDHT.h>  // Knihovna SimpleDHT pro čtení z DHT senzoru
+#include <SimpleDHT.h>
 #include <LittleFS.h>
 #include "password.h"
 
 #define DHTPIN 0         // DHT senzor je připojen na GPIO0 (D3 na Wemos D1 Mini)
 #define WATER_SENSOR_PIN A0  // Pin pro analogový Water Level Sensor
+#define PUMP_PIN 2       // Pin pro spínání pumpy (GPIO2)
 
-SimpleDHT11 dht11(DHTPIN);  // Inicializace DHT11 senzoru
+SimpleDHT11 dht11(DHTPIN);
 
-AsyncWebServer server(80);  // Inicializace asynchronního webového serveru na portu 80
+AsyncWebServer server(80);
 
-unsigned long lastDHTRead = 0;  // Čas posledního čtení DHT senzoru
-const unsigned long dhtDelay = 2000;  // Zpoždění mezi čteními (2 sekundy)
+unsigned long lastDHTRead = 0;
+const unsigned long dhtDelay = 2000;
 float temperature = 0;
 float humidity = 0;
+bool pumpState = false; // Stav pumpy, false = vypnuto, true = zapnuto
 
 void listFiles() {
     Serial.println("Výpis souborů v LittleFS:");
@@ -30,7 +32,9 @@ void listFiles() {
 
 void setup() {
     Serial.begin(115200);
-    pinMode(WATER_SENSOR_PIN, INPUT);  // Nastavení pinu pro Water Level Sensor jako vstup
+    pinMode(WATER_SENSOR_PIN, INPUT);
+    pinMode(PUMP_PIN, OUTPUT); // Nastavení PUMP_PIN jako výstup
+    digitalWrite(PUMP_PIN, LOW); // Výchozí stav pumpy = vypnuto
 
     Serial.print("Připojuji se k WiFi");
     WiFi.begin(ssid, password);
@@ -52,13 +56,12 @@ void setup() {
     Serial.println("LittleFS inicializován.");
     listFiles();
 
+    // Endpoint pro čtení dat a zobrazení hlavní stránky
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         File file = LittleFS.open("/index.html", "r");
         if (file) {
             String response = file.readString();
             file.close();
-
-            
 
             unsigned long currentMillis = millis();
             if (currentMillis - lastDHTRead >= dhtDelay) {
@@ -82,12 +85,19 @@ void setup() {
             response.replace("{{temperature}}", String(temperature));
             response.replace("{{humidity}}", String(humidity));
             response.replace("{{waterLevel}}", String(waterLevel));
-            response.replace("{{soilMoisture}}", "Načítání...");
+            response.replace("{{pumpState}}", pumpState ? "Zapnuto" : "Vypnuto");
 
             request->send(200, "text/html", response);
         } else {
             request->send(404, "text/plain", "404: Not Found");
         }
+    });
+
+    // Endpoint pro manuální zapnutí/vypnutí pumpy
+    server.on("/togglePump", HTTP_GET, [](AsyncWebServerRequest *request) {
+        pumpState = !pumpState;  // Změní stav pumpy
+        digitalWrite(PUMP_PIN, pumpState ? HIGH : LOW);  // Zapne nebo vypne pumpu podle stavu
+        request->send(200, "text/plain", pumpState ? "Pumpa zapnuta" : "Pumpa vypnuta");
     });
 
     server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request) {
